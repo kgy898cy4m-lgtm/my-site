8ad191884686604e94867ce1b54a1eaad58b23bd
kgy898cy4m-lgtm <kgy898cy4m@privaterelay.appleid.com>
2026-01-17 01:14:35 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function add() {
   cart++;
   document.getElementById("count").innerText = cart;
 }
+function getLocation() {
+  if (!navigator.geolocation) {
+    document.getElementById("location").innerText =
+      "Brauzer lokatsiyani qo‘llab-quvvatlamaydi";
+    return;
+  }
+
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+
+      document.getElementById("location").innerText =
+        📍 Latitude: ${lat}, Longitude: ${lon};
+    },
+    () => {
+      document.getElementById("location").innerText =
+        "❌ Lokatsiyaga ruxsat berilmadi";
+    }
+  );
+}
