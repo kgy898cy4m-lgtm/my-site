69101f6795dfd4c88a1f00106f1d2b1756a598c9
kgy898cy4m-lgtm <kgy898cy4m@privaterelay.appleid.com>
2026-01-17 04:31:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,10 @@
-.burger {
-  font-size: 24px;
-  background: none;
-  border: none;
-  cursor: pointer;
-  z-index: 2001;        /* MUHIM */
-  position: relative;  /* MUHIM */
-}
-/* KURSOR BORGANDA */
-.burger:hover {
-  transform: scale(1.15) rotate(-5deg);
-}
-.burger:hover {
-  transform: scale(1.15) translateY(-2px);
-}
-.burger:active {
-  transform: scale(0.95);
-}
 let cart = 0;
 
-/* Savatga qo‘shish */
 function add() {
   cart++;
   document.getElementById("count").innerText = cart;
 }
 
-/* Lokatsiya olish */
-function getLocation() {
-  const el = document.getElementById("location");
-
-  if (!navigator.geolocation) {
-    el.innerText = "Brauzer lokatsiyani qo‘llab-quvvatlamaydi";
-    return;
-  }
-
-  navigator.geolocation.getCurrentPosition(
-    (position) => {
-      const lat = position.coords.latitude.toFixed(5);
-      const lon = position.coords.longitude.toFixed(5);
-
-      el.innerText = 📍 Latitude: ${lat}, Longitude: ${lon};
-    },
-    () => {
-      el.innerText = "❌ Lokatsiyaga ruxsat berilmadi";
-    }
-  );
-}
-
-/* Burger menu */
 function openMenu() {
   document.getElementById("menu").classList.toggle("open");
-}
-
-function closeMenu() {
-  document.getElementById("menu").classList.remove("open");
 }
