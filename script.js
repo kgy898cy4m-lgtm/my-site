65d00f62b6f5ee52cbe74789695ddadc4691acea
kgy898cy4m-lgtm <kgy898cy4m@privaterelay.appleid.com>
2026-01-16 23:53:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const text = document.getElementById("text");
 button.addEventListener("click", function () {
   text.textContent = "🎉 Tugma bosildi!";
 });
+button.addEventListener("click", function () {
+  document.body.style.background = "#d1f7c4";
+});
