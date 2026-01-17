5c6120fe56113ab22833cc6da6d29e080a027826
kgy898cy4m-lgtm <kgy898cy4m@privaterelay.appleid.com>
2026-01-17 14:16:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ function closeMenu() {
   document.getElementById("menu").classList.remove("open");
   document.getElementById("overlay").classList.remove("show");
 }
+const servers = document.querySelectorAll(".service");
+
+servers.forEach(server => {
+  server.addEventListener("click", () => {
+    // oldingi active ni olib tashlaymiz
+    servers.forEach(s => s.classList.remove("active"));
+
+    // bosilganiga active qo‘shamiz
+    server.classList.add("active");
+  });
+});
